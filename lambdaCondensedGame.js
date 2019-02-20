bbcf597fd2328282ccd4ce51a4699c70d4ceebcc
John Pollard <johnp@bravelocation.com>
2019-02-20 14:42:21 +0000

    Fixed up the Lambda request and response

--- lambdaCondensedGame.js
+++ lambdaCondensedGame.js
@@ -5,19 +5,32 @@ exports.handler = function(event, context, callback) {
     // Get the parameters from the post request
     console.log('Received event:', JSON.stringify(event, null, 2));
 
-    if (event.gameDate === undefined || event.team === undefined) {
-        console.log("Input error: " + JSON.stringify(event));
+    console.log('Body:' + event.body);
+
+    const inputParameters = JSON.parse(event.body);
+    const gameDate = inputParameters.params.gameDate;
+    const team = inputParameters.params.team;
+
+    if (gameDate === undefined || team === undefined) {
+        console.log("Input error: " + JSON.stringify(inputParameters));
         callback("400 Invalid Input");
         return;
     }
 
-    mlbApi.findCondensedGame(event.gameDate, event.team.toLowerCase(), function(gameDetails, error){
+    mlbApi.findCondensedGame(gameDate, team.toLowerCase(), function(gameDetails, error){
         if (error) {
             console.log("Error: " + error);
             callback("500 Internal Error");
         } else {
             console.log("Results: " + JSON.stringify(gameDetails));
-            callback(null, gameDetails);
+
+            var response = {
+                "statusCode": 200,
+                "body": JSON.stringify(gameDetails),
+                "isBase64Encoded": false
+            };
+
+            callback(null, response);
         }
     });
 };
